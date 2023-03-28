00d11fe6064a84f2bb5792371b73fbf4f869b55e
hart <100047482+loyallhart@users.noreply.github.com>
2023-03-28 13:29:55 -0400

    update files

---
+++ Develop/index.js
@@ -0,0 +1,87 @@
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+// array of questions for user input to input answers
+//Nmae, Description, Installation, Usage, Contributing, and Tests, 
+const questions = [
+{
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of your project?',
+},
+
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please enter a description of your project',
+},
+
+{
+    type: 'input',
+    name: 'projectName',
+    message: 'What is the name of your project?',
+},
+
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Please tell us the steps to install your project',
+
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'How will this project be used? Please provide screenshots as needed',
+
+},
+
+ {
+    type: 'input',
+    name: 'contributors',
+    message: 'list your contributors and there Github links',
+ },
+
+{ 
+    type: 'input',
+    name:'tests',
+    message: 'Please write test examples for your application',
+},
+
+{   
+    type: 'input',
+    name:'license',
+    message: 'Choose your license',
+    choice: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+
+},
+{
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username:',
+},
+
+{
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email address fir people to contact you with questions:',
+},
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (error) => {
+        if (error) {
+            console.log(error);
+            return;
+    }
+    console.log(`File ${fileName} has been generated!`);
+});
+}
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
